a244443b89a8cfa45e18a113b1c147d37d773e91
Nebreoma <Nebreoma12@gmail.com>
2024-07-05 20:14:20 +0300

    add new code for branch freqlog

--- src/Main.java
+++ src/Main.java
@@ -1,62 +1,89 @@
 import java.util.*;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class Main {
     public static final Map<Integer, Integer> sizeToFreq = new HashMap<>();
     public static final char R = 'R';
-    public static int maxCount = 0, maxInterval;
+    public static int maxInterval, maxCount;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         long startTs = System.currentTimeMillis(); // start time
-        ExecutorService executor = Executors.newFixedThreadPool(1000);
+        List<Thread> threads = new ArrayList<>();
+        Thread thread = new Thread();
+
+        thread = new Thread(() -> {
+            try {
+                while (!Thread.interrupted()) {
+                    synchronized (sizeToFreq) {
+                        sizeToFreq.wait();
+                        for (Map.Entry entry : sizeToFreq.entrySet()) {
+                            if (maxCount < (int) entry.getValue()) {
+                                maxCount = (int) entry.getValue();
+                                maxInterval = (int) entry.getKey();
+                            }
+                            System.out.println("Самое частое количество повторений "
+                                    + maxInterval
+                                    + " (встретилось "
+                                    + maxCount
+                                    + " раз)");
+                        }
+                    }
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+        thread.start();
+
 
         for (int i = 0; i < 1000; i++) { //цикл для потоков
-
-            executor.execute(() -> {
+            Thread thread1000 = new Thread(() -> {
                 String route = generateRoute("RLRFR", 100);
-                //System.out.println (route);
+                //System.out.println(route);
                 int interval, count;
                 int startInterval = -1;
+                synchronized (sizeToFreq) {
+                    for (int j = 0; j < 100; j++) { //цикл найти первую букву R
+                        if (route.charAt(j) == R && startInterval == -1) {
+                            startInterval = j;
+                            continue;
+                        }
+                        if (route.charAt(j) != R && startInterval != -1) { //найти интервал
+                            interval = j - startInterval;
+                            startInterval = -1;
 
-                for (int j = 0; j < 100; j++) { //цикл найти первую букву R
-                    if (route.charAt(j) == R && startInterval == -1) {
-                        startInterval = j;
-                        continue;
-                    }
-                    if (route.charAt(j) != R && startInterval != -1) { //найти интервал
-                        interval = j - startInterval;
-                        startInterval = -1;
-
-                        //добавить интервал и его повторение в map
-                        synchronized (sizeToFreq) {
+                            //добавить интервал и его повторение в map
                             if (sizeToFreq.containsKey(interval)) {
                                 count = sizeToFreq.get(interval) + 1;
                             } else {
                                 count = 1;
                             }
-                            if (maxCount < count) {
-                                maxCount = count;
-                                maxInterval = interval;
-                            }
                             sizeToFreq.put(interval, count);
+                            sizeToFreq.notify();
                         }
                     }
                 }
-            });//конец лямбды
+            });
+            threads.add(thread1000);
+            thread1000.start();
+        }//конец циклов 1000
+
+        for (Thread t : threads) {
+            t.join();
         }
-        executor.shutdown();
+
+        thread.interrupt();
 
         long endTs = System.currentTimeMillis(); // end time
         System.out.println("Time: " + (endTs - startTs) + "ms");
-
-        System.out.println("Самое частое количество повторений " + maxInterval + " (встретилось " + maxCount + " раз)\nДругие размеры:");
-        for (Map.Entry entry : sizeToFreq.entrySet()) {
-            if (maxInterval == (int) entry.getKey()) {
-                continue;
+        synchronized (sizeToFreq) {
+            System.out.println("Другие размеры:");
+            for (Map.Entry entry : sizeToFreq.entrySet()) {
+                if (maxInterval == (int) entry.getKey()) {
+                    continue;
+                }
+                System.out.println("- " + entry.getKey() + " (" + entry.getValue() + " раз)");
             }
-            System.out.println("- " + entry.getKey() + " (" + entry.getValue() + " раз)");
         }
 
     }//конец main
@@ -72,4 +99,5 @@ public class Main {
     }
 }
 
-//Time: 67ms
+//Время в main: 67 ms
+//Время в sizeToFreq: 76 ms
